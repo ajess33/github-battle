4d4559d5bd301f4ef6a00fffbdc6152c5b86f1b9
Austin Jess <ajess33@gmail.com>
2018-05-07 21:44:51 -0500

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import Popular from './components/Popular';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Battle from './components/Battle';
+
 import './index.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route
+              render={() => {
+                return <p>Not Found</p>;
+              }}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
