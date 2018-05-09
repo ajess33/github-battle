9fe399a0cc038bc2c7820dc381a1ed4a89fa8dea
Austin Jess <ajess33@gmail.com>
2018-05-09 15:22:55 -0500

    Forms and Encapsulation

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,8 +1,103 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// Dont understand why we need to listen for changes to the input when we aren't using the input until the the submit button is clicked
+
+class PlayerInput extends React.Component {
+  state = {
+    username: ''
+  };
+
+  handleChange = (event) => {
+    const value = event.target.value;
+
+    this.setState(() => {
+      return {
+        username: value
+      };
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.props.id, this.state.username);
+  };
+
+  render() {
+    return (
+      <form className="column" onSubmit={this.handleSubmit}>
+        <label className="header" htmlFor="username">
+          {this.props.label}
+        </label>
+        <input
+          id="username"
+          placeholder="github"
+          type="text"
+          autoComplete="off"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button
+          className="button"
+          type="submit"
+          disabled={!this.state.username}
+        >
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
 
 class Battle extends React.Component {
+  state = {
+    playerOneName: '',
+    playerTwoName: '',
+    playerOneImage: null,
+    playerTwoImage: null
+  };
+
+  // If ID is player one, then update the player one username and image, otherwise update player two's username and image
+  handleSubmit = (id, username) => {
+    this.setState(() => {
+      let newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = `https://github.com/${username}.png?size=200`;
+      return newState;
+    });
+  };
   render() {
-    return <div>BATTLE!</div>;
+    const playerOneName = this.state.playerOneName;
+    const playerTwoName = this.state.playerTwoName;
+
+    return (
+      <div>
+        <div className="row">
+          {!playerOneName && (
+            <PlayerInput
+              id="playerOne"
+              label="Player One"
+              onSubmit={this.handleSubmit}
+            />
+          )}
+
+          {!playerTwoName && (
+            <PlayerInput
+              id="playerTwo"
+              label="Player Two"
+              onSubmit={this.handleSubmit}
+            />
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
